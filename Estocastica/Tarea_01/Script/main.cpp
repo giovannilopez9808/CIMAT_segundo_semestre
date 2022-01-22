7225448facb4a9e665b36737a44c53626456aa65
giovannilopez9808 <giovannilopez9808@gmail.com>
2022-01-21 23:20:52 -0600

    Add min save on a file

--- Estocastica/Tarea_01/Script/main.cpp
+++ Estocastica/Tarea_01/Script/main.cpp
@@ -4,19 +4,25 @@
 int main()
 {
     unsigned seed = 12345625;
-    int channels = 54;
-    int min = 10000000000;
-    // std::string filename_output = "../Output/test.txt";
+    int channels = 34;
+    long int cost, min;
+    int iterations = 100;
+    std::string filename_output = "../Output/test.txt";
     std::string filename_data = "../Data/GSM2-272.ctr";
-    // std::fstream file_output = open_file(filename_output);
+    std::fstream file_output = open_file(filename_output);
     lines_class lines(filename_data);
-    for (int i = 0; i < 1000000; i++)
+    min = 100000000000000000;
+    for (int i = 0; i < iterations; i++)
     {
+        std::cout << i << " de " << iterations << std::endl;
         seed = rand();
-        long int cost = lines.cost(seed,
-                                   channels);
-        std::cout << cost << std::endl;
-        // file_output.close();
+        cost = lines.cost(seed,
+                          channels);
+        if (min > cost)
+        {
+            min = cost;
+        }
     }
+    file_output << min << std::endl;
     return 0;
 }
