d94340d3691480621574863c370e8d605d74d892
giovannilopez9808 <giovannilopez9808@gmail.com>
2022-01-22 20:30:12 -0600

    Add main arguments

--- Estocastica/Tarea_01/Script/main.cpp
+++ Estocastica/Tarea_01/Script/main.cpp
@@ -1,20 +1,21 @@
 #include "Modules/data.h"
 #include "Modules/nodes.h"
 #include "Modules/files.h"
-int main()
+int main(int argc, char* argv[])
 {
-    unsigned seed = 12345625;
-    int channels = 34;
+    if(argc==5)
+    {
+    std::string filename_data = argv[1];
+    std::string filename_output = argv[2];
+    unsigned seed =atoi(argv[3]);
+    int channels = atoi(argv[4]);
     long int cost, min;
-    int iterations = 100;
-    std::string filename_output = "../Output/test.txt";
-    std::string filename_data = "../Data/GSM2-272.ctr";
+    int iterations = 2;
     std::fstream file_output = open_file(filename_output);
     lines_class lines(filename_data);
     min = 100000000000000000;
     for (int i = 0; i < iterations; i++)
     {
-        std::cout << i << " de " << iterations << std::endl;
         seed = rand();
         cost = lines.cost(seed,
                           channels);
@@ -24,5 +25,9 @@ int main()
         }
     }
     file_output << min << std::endl;
+    }else
+    {
+        std::cout << "Faltan argumentos" << std::endl;
+    }
     return 0;
 }
