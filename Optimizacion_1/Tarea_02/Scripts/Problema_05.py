401b4daa7742b1fee8968b2463c08be29f9aff84
giovannilopez9808 <giovannilopez9808@gmail.com>
2022-02-07 00:36:38 -0600

    Problema 5

---
+++ Optimizacion_1/Tarea_02/Scripts/Problema_05.py
@@ -0,0 +1,52 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def function(x1, x2):
+    return 8 * x1 + 12 * x2 + x1**2 - 2 * x2**2
+
+
+parameters = {0: {"color": "#390099",
+                  "level": -10,
+                  },
+              1: {"color": "#9e0059",
+                  "level": -5,
+                  },
+              2: {"color": "#ff0054",
+                  "level": 0,
+                  },
+              3: {"color": "#ff5400",
+                  "level": 5,
+                  },
+              4: {"color": "#ffbd00",
+                  "level": 10,
+                  }}
+
+x1 = np.linspace(-12, 4, 1000)
+x2 = np.linspace(-2, 8, 1000).reshape(-1, 1)
+z = function(x1, x2)
+x1, x2 = np.meshgrid(x1, x2)
+plots = []
+labels = []
+for parameter in parameters:
+    dataset = parameters[parameter]
+    label = "level={}".format(dataset["level"])
+    cs = plt.contour(x1,
+                     x2,
+                     z,
+                     linestyles="-",
+                     levels=[dataset["level"]],
+                     colors=dataset["color"])
+    legend, _ = cs.legend_elements()
+    plots += legend
+    labels += [label]
+plt.legend(plots,
+           labels,
+           ncol=len(plots),
+           frameon=False,
+           bbox_to_anchor=(1.12, 1.1))
+plt.grid(ls="--",
+         color="#000000",
+         alpha=0.5,)
+plt.tight_layout()
+plt.show()
