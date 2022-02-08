aa382d7f49762f56a38a14ed20b2568d7b6404c7
giovannilopez9808 <giovannilopez9808@gmail.com>
2022-02-08 12:44:44 -0600

    Join scripts

--- Optimizacion_1/Tarea_02/Scripts/Problema_06.py
+++ Optimizacion_1/Tarea_02/Scripts/Problema_06.py
@@ -7,6 +7,26 @@ def function(x: np.array, y: np.array) -> np.array:
     return f
 
 
+def dx1(x1, x2):
+    return -200*(x2-x1**2)*2*x1-2*(1-x1)*x1
+
+
+def dx2(x1, x2):
+    return 200*(x2-x1**2)
+
+
+def dx1x1(x1, x2):
+    return 1200*x1**2+4*x1-400*x2-2
+
+
+def dx2x2(x1, x2):
+    return 200
+
+
+def dx1x2(x1, x2):
+    return -400*x1
+
+
 parameters = {
     0: {
         "color": "#ffbd00",
@@ -61,3 +81,16 @@ plt.legend(plots,
 #          alpha=0.5)
 plt.tight_layout()
 plt.savefig("../Document/Graphics/problem06.png")
+
+
+values = [[0, 0], [1, 1]]
+for value in values:
+    x, y = value
+    print("------------------------------------------------------------")
+    print("(x,y):\t({},{})".format(x, y))
+    print("dxx:{: >20}".format(dx1x1(x, y)))
+    print("dyy:{: >20}".format(dx2x2(x, y)))
+    print("dxy:{: >20}".format(dx1x2(x, y)))
+    print("dxx*dxy-dxy**2:{: >9}".format(
+        dx1x1(x, y) * dx2x2(x, y) - (dx1x2(x, y)**2)))
+    print("------------------------------------------------------------\n")
