f5f5f7bf839a843c7b7f7815943787269dd9cebd
giovannilopez9808 <giovannilopez9808@gmail.com>
2022-02-20 14:57:22 -0600

    Update graphics

--- Patrones/Tarea_02/Scripts/Problema_3_2/Graphics_histogram_rating.py
+++ Patrones/Tarea_02/Scripts/Problema_3_2/Graphics_histogram_rating.py
@@ -5,23 +5,34 @@ parameters = obtain_parameters()
 colors = obtain_colors()
 parameters["graphics file"] = "Histogram_rating.png"
 ratings = read_rating_list()
+ratings = list(ratings["rating"])
 labels = obtain_all_scores()
 yticks = np.linspace(0, 30000, 13)
-height, values, hist_list = plt.hist(ratings["rating"],
-                                     bins=10,
-                                     alpha=0.7,
-                                     rwidth=0.85)
-for x, y, label in zip(values, height, labels):
-    plt.text(x+0.125, y+100, "{}".format(label))
-for color, i in zip(colors, range(len(hist_list))):
-    hist_list[i].set_facecolor(color)
-plt.xticks([])
-plt.yticks(yticks)
+set_rating = set(ratings)
+data = pd.DataFrame(0,
+                    index=set_rating,
+                    columns=["Count"])
+for rating in set_rating:
+    data["Count"][rating] = ratings.count(rating)
+bars = plt.bar(data.index,
+               list(data["Count"]),
+               alpha=0.7,
+               width=0.4
+               )
+for x, y in zip(data.index, data["Count"]):
+    plt.text(x-0.2, y+100, "{:.0f}".format(y))
+for color, i in zip(colors, range(len(data))):
+    bars[i].set_facecolor(color)
+plt.xlabel("Calificaciones")
+plt.xticks(data.index)
+plt.yticks(yticks
+           )
 plt.ylim(0, 30000)
 plt.ylabel("Conteo de calificación")
 plt.grid(ls="--",
          color="#000000",
-         alpha=0.7)
+         alpha=0.7,
+         axis="y")
 plt.tight_layout()
 plt.savefig(join_path(parameters["path graphics"],
                       parameters["graphics file"]))
