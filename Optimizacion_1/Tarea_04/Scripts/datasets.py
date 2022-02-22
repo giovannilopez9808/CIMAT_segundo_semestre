cdb86111ccb7872941d36279156159a6e4a6de9a
giovannilopez9808 <giovannilopez9808@gmail.com>
2022-02-22 16:31:05 -0600

    Update datasets

--- Optimizacion_1/Tarea_04/Scripts/datasets.py
+++ Optimizacion_1/Tarea_04/Scripts/datasets.py
@@ -2,8 +2,9 @@ def obtain_dataset(name: str) -> dict:
     datasets = {
         "rosembrock 2 predefined newton": {"path results": "../Results/",
                                            "n": 2,
+                                           "c1": 0.1,
+                                           "c2": 0.9,
                                            "tau": 1e-6,
-                                           "alpha": 1e-4,
                                            "problem name": "rosembrock",
                                            "initial point": "predefined",
                                            "algorithm name": "newton"
@@ -11,13 +12,15 @@ def obtain_dataset(name: str) -> dict:
         "rosembrock 2 predefined gradient": {"path results": "../Results/",
                                              "n": 2,
                                              "tau": 1e-6,
-                                             "alpha": 1e-4,
+                                             "alpha": 1e-3,
                                              "problem name": "rosembrock",
                                              "initial point": "predefined",
                                              "algorithm name": "descent gradient"
                                              },
         "rosembrock 2 random newton": {"path results": "../Results/",
                                        "n": 2,
+                                       "c1": 0.1,
+                                       "c2": 0.9,
                                        "tau": 1e-7,
                                        "alpha": 1e-4,
                                        "problem name": "rosembrock",
@@ -26,8 +29,8 @@ def obtain_dataset(name: str) -> dict:
                                        },
         "rosembrock 2 random gradient": {"path results": "../Results/",
                                          "n": 2,
-                                         "tau": 1e-7,
-                                         "alpha": 1e-4,
+                                         "tau": 1e-6,
+                                         "alpha": 1e-3,
                                          "problem name": "rosembrock",
                                          "initial point": "random",
                                          "algorithm name": "descent gradient"
@@ -66,8 +69,9 @@ def obtain_dataset(name: str) -> dict:
                                          },
         "wood 4 random newton": {"path results": "../Results/",
                                  "n": 4,
-                                 "tau": 1e-6,
-                                 "alpha": 1e-4,
+                                 "c1": 0.1,
+                                 "c2": 0.9,
+                                 "tau": 1e-12,
                                  "problem name": "wood",
                                  "initial point": "random",
                                  "algorithm name": "newton"
@@ -85,7 +89,6 @@ def obtain_dataset(name: str) -> dict:
                                      "c1": 0.1,
                                      "c2": 0.9,
                                      "tau": 1e-6,
-                                     "alpha": 1e-3,
                                      "problem name": "wood",
                                      "initial point": "predefined",
                                      "algorithm name": "newton"
@@ -96,7 +99,7 @@ def obtain_dataset(name: str) -> dict:
                                        "alpha": 1e-4,
                                        "problem name": "wood",
                                        "initial point": "predefined",
-                                       "algorithm name": "newton"
+                                       "algorithm name": "descent gradient"
                                        }
     }
     return datasets[name]
