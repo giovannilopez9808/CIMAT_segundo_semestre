b66f94343734679060f18debc743b3aa19c7fc00
giovannilopez9808 <giovannilopez9808@gmail.com>
2022-02-24 21:05:03 -0600

    Merge datasrts

--- Optimizacion_1/Tarea_04/Scripts/Problema_2/datasets.py
+++ Optimizacion_1/Tarea_04/Scripts/Problema_2/datasets.py
@@ -107,3 +107,123 @@ class datasets_class:
 
     def obtain_dataset(self, name: str) -> dict:
         return self.datasets[name]
+
+
+class datasets_statistics_class:
+    def __init__(self) -> None:
+        self.datasets = {
+            "wood 4 newton": {
+                "path results": "../../Results/Problema_2/Random_results/",
+                "path graphics": "../../Graphics/Problema_2/Random_results/",
+                "filename": "wood_4_newton.csv",
+                "filename graphics": "wood_4_newton.png",
+            },
+            "wood 4 gradient": {
+                "path results": "../../Results/Problema_2/Random_results/",
+                "path graphics": "../../Graphics/Problema_2/Random_results/",
+                "filename": "wood_4_gradient.csv",
+                "filename graphics": "wood_4_gradient.png"
+            },
+            "rosembrock 2 newton": {
+                "path results": "../../Results/Problema_2/Random_results/",
+                "path graphics": "../../Graphics/Problema_2/Random_results/",
+                "filename": "rosembrock_2_newton.csv",
+                "filename graphics": "rosembrock_2_newton.png"
+            },
+            "rosembrock 2 gradient": {
+                "path graphics": "../../Graphics/Problema_2/Random_results/",
+                "path results": "../../Results/Problema_2/Random_results/",
+                "filename": "rosembrock_2_gradient.csv",
+                "filename graphics": "rosembrock_2_gradient.png",
+            },
+            "rosembrock 100 newton": {
+                "path graphics": "../../Graphics/Problema_2/Random_results/",
+                "path results": "../../Results/Problema_2/Random_results/",
+                "filename": "rosembrock_100_newton.csv",
+                "filename graphics": "rosembrock_100_newton.png"
+            },
+            "rosembrock 100 gradient": {
+                "path graphics": "../../Graphics/Problema_2/Random_results/",
+                "path results": "../../Results/Problema_2/Random_results/",
+                "filename": "rosembrock_100_gradient.csv",
+                "filename graphics": "rosembrock_100_gradient.png",
+            }}
+
+    def obtain_dataset(self, name: str) -> dict:
+        return self.datasets[name]
+
+
+class graphics_datasets_class:
+
+    def __init__(self) -> None:
+        self.datasets = {
+            "wood 4 predefined": {
+                "path data": "../../Results/Problema_2/",
+                "path graphics": "../../Graphics/Problema_2/",
+                "dataset": "wood",
+                "initial point": "predefined",
+                "n": 4,
+                "xlim": [0, 50],
+                "xticks": [x for x in range(0, 55, 5)],
+                "y1 lim": [0, 4000],
+                "y2 lim": [0, 3000],
+            },
+            "wood 4 random": {
+                "path data": "../../Results/Problema_2/",
+                "path graphics": "../../Graphics/Problema_2/",
+                "dataset": "wood",
+                "initial point": "random",
+                "n": 4,
+                "xlim": [0, 100],
+                "xticks": [x for x in range(0, 105, 5)],
+                "y1 lim": [0, 150],
+                "y2 lim": [0, 500],
+            },
+            "rosembrock 2 predefined": {
+                "path data": "../../Results/Problema_2/",
+                "path graphics": "../../Graphics/Problema_2/",
+                "dataset": "rosembrock",
+                "initial point": "predefined",
+                "n": 2,
+                "xlim": [0, 50],
+                "xticks": [x for x in range(0, 55, 5)],
+                "y1 lim": [0, 30],
+                "y2 lim": [0, 300],
+            },
+            "rosembrock 2 random": {
+                "path data": "../../Results/Problema_2/",
+                "path graphics": "../../Graphics/Problema_2/",
+                "dataset": "rosembrock",
+                "initial point": "random",
+                "n": 2,
+                "xlim": [0, 100],
+                "xticks": [x for x in range(0, 105, 5)],
+                "y1 lim": [0, 30],
+                "y2 lim": [0, 300],
+            },
+            "rosembrock 100 predefined": {
+                "path data": "../../Results/Problema_2/",
+                "path graphics": "../../Graphics/Problema_2/",
+                "dataset": "rosembrock",
+                "initial point": "predefined",
+                "n": 100,
+                "xlim": [0, 100],
+                "xticks": [x for x in range(0, 105, 5)],
+                "y1 lim": [0, 30],
+                "y2 lim": [0, 300],
+            },
+            "rosembrock 100 random": {
+                "path data": "../../Results/Problema_2/",
+                "path graphics": "../../Graphics/Problema_2/",
+                "dataset": "rosembrock",
+                "initial point": "random",
+                "n": 100,
+                "xlim": [0, 200],
+                "xticks": [x for x in range(0, 220, 20)],
+                "y1 lim": [0, 200],
+                "y2 lim": [0, 300],
+            }
+        }
+
+    def obtain_dataset(self, name: str) -> dict:
+        return self.datasets[name]
