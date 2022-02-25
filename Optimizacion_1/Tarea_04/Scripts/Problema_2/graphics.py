e4d2e42fd37ad91e8ff5cfa590ea0a967eb1ebcd
giovannilopez9808 <giovannilopez9808@gmail.com>
2022-02-24 21:05:11 -0600

    Update

--- Optimizacion_1/Tarea_04/Scripts/Problema_2/graphics.py
+++ Optimizacion_1/Tarea_04/Scripts/Problema_2/graphics.py
@@ -1,30 +1,23 @@
-from datasets_graphics import *
+from datasets import *
 import matplotlib.pyplot as plt
 from os import listdir as ls
 from auxiliar import *
 
 datasets = graphics_datasets_class()
-parameters = datasets.obtain_dataset("rosembrock 2 predefined")
+parameters = datasets.obtain_dataset("rosembrock 100 random")
 files = ls(parameters["path data"])
 graphics_name = obtain_graphics_filename(parameters)
-files = obtain_files_per_dataset(files,
-                                 parameters)
+files = obtain_files_per_dataset(files, parameters)
 colors = obtain_colors_per_method()
-fig, (ax1, ax2) = plt.subplots(2, 1,
-                               sharex=True,
-                               figsize=(6, 5))
+fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, figsize=(6, 5))
 for file in files:
     method = obtain_method_from_name(file)
     color = colors[method]
-    filename = join_path(parameters["path data"],
-                         file)
+    filename = join_path(parameters["path data"], file)
     data = read_data(filename)
-    ax1.set_xlim(parameters["xlim"][0],
-                 parameters["xlim"][1])
-    ax1.set_ylim(parameters["y1 lim"][0],
-                 parameters["y1 lim"][1])
-    ax2.set_ylim(parameters["y2 lim"][0],
-                 parameters["y2 lim"][1])
+    ax1.set_xlim(parameters["xlim"][0], parameters["xlim"][1])
+    ax1.set_ylim(parameters["y1 lim"][0], parameters["y1 lim"][1])
+    ax2.set_ylim(parameters["y2 lim"][0], parameters["y2 lim"][1])
     ax1.plot(data.index,
              data["fx"],
              label=method,
@@ -37,12 +30,8 @@ for file in files:
              color=color,
              marker=".",
              ls="--")
-    ax1.grid(ls="--",
-             alpha=0.5,
-             color="#000000")
-    ax2.grid(ls="--",
-             alpha=0.5,
-             color="#000000")
+    ax1.grid(ls="--", alpha=0.5, color="#000000")
+    ax2.grid(ls="--", alpha=0.5, color="#000000")
     ax1.set_ylabel("$f\;(X)$")
     ax2.set_ylabel("$\\nabla f\;(X)$")
     ax2.set_xlabel("Iteraciones")
@@ -54,6 +43,4 @@ fig.legend(frameon=False,
            ncol=2,
            bbox_to_anchor=(0.8, 1.01))
 plt.tight_layout(pad=2)
-plt.savefig(join_path(parameters["path graphics"],
-                      graphics_name),
-            dpi=400)
+plt.savefig(join_path(parameters["path graphics"], graphics_name), dpi=400)
