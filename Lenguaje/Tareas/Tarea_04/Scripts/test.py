63b481866611efdcaa7b65dd57ff823b476c4927
giovannilopez9808 <giovannilopez9808@gmail.com>
2022-03-07 20:13:43 -0600

    First section

--- Lenguaje/Tareas/Tarea_04/Scripts/test.py
+++ Lenguaje/Tareas/Tarea_04/Scripts/test.py
@@ -3,4 +3,9 @@ from Modules.tweets import tweets_data
 
 parameters = obtain_parameters()
 tweets = tweets_data(parameters)
-tweets.obtain_perplexity()
+print("-"*40)
+print("Datos de prueba")
+tweets.obtain_perplexity(use_data_test=True)
+print("-"*40)
+print("Datos de validación")
+tweets.obtain_perplexity(use_data_test=False)
