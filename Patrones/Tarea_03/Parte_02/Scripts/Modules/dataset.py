643707f49dc9b19c672457c51a1e75668eadc9f8
giovannilopez9808 <giovannilopez9808@gmail.com>
2022-03-10 14:10:41 -0600

    Refractoe

--- Patrones/Tarea_03/Parte_02/Scripts/Modules/dataset.py
+++ Patrones/Tarea_03/Parte_02/Scripts/Modules/dataset.py
@@ -4,16 +4,29 @@ from numpy import loadtxt
 
 
 class data_class:
+    """
+    Lectura y organización de base de datos de animals with attributes
+    ------
+    Inputs:
+    + parameters -> diccionario con los parametros de la localizacion y nombre de cada archivo. Estos pueden ser modificados en la funcion obtain_parameters
+    """
+
     def __init__(self, parameters: dict) -> None:
         self.parameters = parameters
         self.read()
 
     def read(self):
+        """
+        Lectura de la matriz de atributos y nombres de los animales
+        """
         self.read_matrix()
         self.read_classes()
         self.len = len(self.colors)
 
     def read_matrix(self):
+        """
+        Lectura de la matriz de atributos
+        """
         filename = join_path(self.parameters["path data"],
                              self.parameters["matrix file"])
         data = loadtxt(filename)
--- Patrones/Tarea_03/Parte_02/Scripts/SOM.py
+++ Patrones/Tarea_03/Parte_02/Scripts/SOM.py
@@ -3,38 +3,27 @@ from Modules.functions import obtain_parameters
 from Modules.dataset import data_class
 from Modules.graphics import plot
 
+models_set = {"LLE": {"model": LLE_model_class(),
+                      "file graphics": "SOM_LLE.png"},
+              "Isomap": {"model": TSNE_model_class(),
+                         "file graphics": "SOM_isomap.png"},
+              "TSNE": {"model": Isomap_model_class(),
+                       "file graphics": "SOM_TSNE.png"}}
 parameters = obtain_parameters()
-parameters["file graphics"] = "SOM_LLE.png"
 data = data_class(parameters)
 SOM_model = SOM_model_class()
 SOM_model.run(data.matrix)
 SOM_model.create_classes_dataframe(data.names)
 print(SOM_model.classes)
-LLE_model = LLE_model_class()
-LLE_model.run(data.matrix)
-plot(LLE_model,
-     data.names,
-     parameters,
-     include_images=False,
-     alpha=1,
-     color=SOM_model.results)
 
-parameters["file graphics"] = "SOM_TSNE.png"
-TSNE_model = TSNE_model_class()
-TSNE_model.run(data.matrix)
-plot(TSNE_model,
-     data.names,
-     parameters,
-     include_images=False,
-     alpha=1,
-     color=SOM_model.results)
-
-parameters["file graphics"] = "SOM_isomap.png"
-Isomap_model = Isomap_model_class()
-Isomap_model.run(data.matrix)
-plot(Isomap_model,
-     data.names,
-     parameters,
-     include_images=False,
-     alpha=1,
-     color=SOM_model.results)
+for model_name in models_set:
+    model_set = models_set[model_name]
+    parameters["file graphics"] = model_set["file graphics"]
+    model = model_set["model"]
+    model.run(data.matrix)
+    plot(model,
+         data.names,
+         parameters,
+         include_images=False,
+         alpha=1,
+         color=SOM_model.results)
