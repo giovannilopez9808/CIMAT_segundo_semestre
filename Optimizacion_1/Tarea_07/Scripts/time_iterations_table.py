84d687567f9a50115fe346c0cc042376a288f2dc
giovannilopez9808 <giovannilopez9808@gmail.com>
2022-04-07 08:31:50 -0600

    Refractor

--- Optimizacion_1/Tarea_07/Scripts/time_iterations_table.py
+++ Optimizacion_1/Tarea_07/Scripts/time_iterations_table.py
@@ -1,18 +1,18 @@
 from Modules.params import get_datasets, get_params, obtain_filename_iteration, obtain_path, join
-from tabulate import tabulate
 from pandas import DataFrame, read_csv
-
+from tabulate import tabulate
 
 params = get_params()
 datasets = get_datasets()
 columns = ["Time", "Function", "Iterations"]
+header = ["Funcion"]
+header += [step_model
+           for step_model in datasets["step models"]]
 for column in columns:
     print("-"*50)
     print("\t\t", column)
     print("-"*50)
     table = []
-    header = ["Funcion"]
-    header += [step_model for step_model in datasets["step models"]]
     for function in datasets["functions"]:
         data_function = [function]
         for step_model in datasets["step models"]:
@@ -20,12 +20,12 @@ for column in columns:
                 "function": function,
                 "step model": step_model,
             }
-            path = obtain_path(params, dataset)
+            path = obtain_path(params,
+                               dataset)
             filename = join(path,
                             params["file stadistics"])
             data = read_csv(filename)
             mean = data[column].mean()
             data_function += [mean]
         table += [data_function]
-    print(tabulate(table,
-                   headers=header))
+    print(tabulate(table, headers=header))
