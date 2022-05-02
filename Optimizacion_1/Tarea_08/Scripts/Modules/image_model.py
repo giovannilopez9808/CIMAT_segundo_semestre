4589cb0a47d1d3eb10193b9e5efe0506ca18e97c
giovannilopez9808 <giovannilopez9808@gmail.com>
2022-05-02 12:10:56 -0500

    Image model works for histograms

--- Optimizacion_1/Tarea_08/Scripts/Modules/image_model.py
+++ Optimizacion_1/Tarea_08/Scripts/Modules/image_model.py
@@ -6,11 +6,12 @@ class image_model:
     def __init__(self) -> None:
         self.red = 1
         self.blue = 2
+        self.epsilon = 0.01
 
     def _rgb_to_class(self, rgb, n_bins):
-        x = int(rgb[0] / 256.0 * n_bins)
-        y = int(rgb[1] / 256.0 * n_bins)
-        z = int(rgb[2] / 256.0 * n_bins)
+        x = int(rgb[0]*n_bins // 256.0)
+        y = int(rgb[1]*n_bins // 256.0)
+        z = int(rgb[2]*n_bins // 256.0)
         return array([x, y, z])
 
     def _function(self, alpha: float, mu: float, c: float, sigma: float) -> array:
@@ -19,36 +20,34 @@ class image_model:
         s = alpha * f_exp
         return sum(s)
 
-    def _F1(self, c: float, alpha_1: array, mu_1: array, alpha_2: array, mu_2: array, epsilon: float, sigma: float) -> float:
-        t1 = self._function(alpha_1, mu_1, c, sigma) + epsilon
+    def _F1(self, c: float, alpha_1: array, mu_1: array, alpha_2: array, mu_2: array, sigma: float) -> float:
+        t1 = self._function(alpha_1, mu_1, c, sigma) + self.epsilon
         t2 = self._function(alpha_1, mu_1, c, sigma)
-        t2 += self._function(alpha_2, mu_2, c, sigma) + 2.0 * epsilon
+        t2 += self._function(alpha_2, mu_2, c, sigma) + 2.0 * self.epsilon
         return t1/t2
 
-    def _F2(self, c: float, alpha_1: array, mu_1: array, alpha_2: array, mu_2: array, epsilon: float, sigma: float) -> float:
-        t1 = self._function(alpha_2, mu_2, c, sigma) + epsilon
+    def _F2(self, c: float, alpha_1: array, mu_1: array, alpha_2: array, mu_2: array, sigma: float) -> float:
+        t1 = self._function(alpha_2, mu_2, c, sigma) + self.epsilon
         t2 = self._function(alpha_1, mu_1, c, sigma)
-        t2 += self._function(alpha_2, mu_2, c, sigma) + 2.0 * epsilon
+        t2 += self._function(alpha_2, mu_2, c, sigma) + 2.0 * self.epsilon
         return t1/t2
 
-    def _get_c_label(self, c: float, alpha_1: array, mu_1: array, alpha_2: array, mu_2: array, epsilon: float, sigma: float) -> float:
+    def _get_c_label(self, c: float, alpha_1: array, mu_1: array, alpha_2: array, mu_2: array,  sigma: float) -> float:
         f1 = self._F1(c,
                       alpha_1,
                       mu_1,
                       alpha_2,
                       mu_2,
-                      epsilon,
                       sigma)
         f2 = self._F2(c,
                       alpha_1,
                       mu_1,
                       alpha_2,
                       mu_2,
-                      epsilon,
                       sigma)
         return self.red if f1 < f2 else self.blue
 
-    def _get_c_labels(self, nbins: int, alpha_1: array, mu_1: array, alpha_2: array, mu_2: array, epsilon: float, sigma: float) -> array:
+    def _get_c_labels(self, nbins: int, alpha_1: array, mu_1: array, alpha_2: array, mu_2: array, sigma: float) -> array:
         labels = zeros((nbins,
                         nbins,
                         nbins),
@@ -62,12 +61,11 @@ class image_model:
                                               mu_1,
                                               alpha_2,
                                               mu_2,
-                                              epsilon=epsilon,
-                                              sigma=sigma)
+                                              sigma)
                     labels[i][j][k] = label
         return labels
 
-    def segmentation(self, img: array, nbins: int, alpha: array, mu: array, epsilon: float, sigma: float) -> array:
+    def segmentation(self, img: array, nbins: int, alpha: array, mu: array, sigma: float) -> array:
         alpha_1, alpha_2 = alpha
         mu_1, mu_2 = mu
         labels = self._get_c_labels(nbins,
@@ -75,7 +73,6 @@ class image_model:
                                     mu_1,
                                     alpha_2,
                                     mu_2,
-                                    epsilon=epsilon,
                                     sigma=sigma)
         n = img.shape[0]
         m = img.shape[1]
@@ -90,31 +87,25 @@ class image_model:
                 img_seg[i][j][2] = 255 if label == self.blue else 0
         return img_seg
 
-    def _H1(self, h1: array, h2: array, c: array, epsilon: float = 0.01) -> float:
-        t1 = h1[c[0]][c[1]][c[2]] + epsilon
-        t2 = h1[c[0]][c[1]][c[2]] + h2[c[0]][c[1]][c[2]] + 2.0 * epsilon
-        return t1/t2
+    def _H_function(self, h: array, c: array) -> float:
+        h = h[c[0], c[1], c[2]]
+        return h
 
-    def _H2(self, h1: array, h2: array, c: array, epsilon: float = 0.01) -> float:
-        t1 = h2[c[0]][c[1]][c[2]] + epsilon
-        t2 = h1[c[0]][c[1]][c[2]] + h2[c[0]][c[1]][c[2]] + 2.0 * epsilon
-        return t1/t2
-
-    def _get_c_label_h(self, c: array, hist_0: array, hist_1: array) -> float:
-        h1 = self._H1(hist_0, hist_1, c)
-        h2 = self._H2(hist_0, hist_1, c)
-        return self.blue if h1 < h2 else self.red
+    def _get_c_label_h(self, hist_0: array, hist_1: array, c: array) -> float:
+        h1 = self._H_function(hist_0, c)
+        h2 = self._H_function(hist_1, c)
+        down = h1+h2 + 2.0 * self.epsilon
+        h1 = (h1+self.epsilon)/down
+        h2 = (h2+self.epsilon)/down
+        return self.red if h1 < h2 else self.blue
 
     def _get_c_labels_h(self, nbins: int, hist_0: array, hist_1: array) -> array:
-        labels = zeros((nbins,
-                        nbins,
-                        nbins),
-                       dtype=int)
+        labels = zeros((nbins, nbins, nbins), dtype=int)
         for i in range(nbins):
             for j in range(nbins):
                 for k in range(nbins):
                     c = array([i, j, k])
-                    labels[i][j][k] = self._get_c_label_h(c, hist_0, hist_1)
+                    labels[i, j, k] = self._get_c_label_h(hist_0, hist_1, c)
         return labels
 
     def histogram_segmentation(self, img: array, nbins: int, hist_1: array, hist_2: array) -> array:
@@ -126,9 +117,9 @@ class image_model:
         img_seg = img.copy()
         for i in range(n):
             for j in range(m):
-                c = self._rgb_to_class(img[i][j], nbins)
-                label = labels[c[0], c[1], c[2]]
-                img_seg[i][j][0] = 255 if label == self.blue else 0
-                img_seg[i][j][1] = 0
-                img_seg[i][j][2] = 255 if label == self.red else 0
+                c = self._rgb_to_class(img[i, j], nbins)
+                label = labels[c[0]][c[1]][c[2]]
+                img_seg[i, j, 0] = 255 if label == self.red else 0
+                img_seg[i, j, 1] = 0
+                img_seg[i, j, 2] = 255 if label == self.blue else 0
         return img_seg
--- Optimizacion_1/Tarea_08/Scripts/test.py
+++ Optimizacion_1/Tarea_08/Scripts/test.py
@@ -6,33 +6,30 @@ import cv2
 
 
 def plot_img(img1, img2, t='Original', s='Segmentation'):
-    fig = plt.figure(figsize=(8, 8))
-    rows = 1
-    columns = 2
-    ax1 = fig.add_subplot(rows, columns, 1)
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 8))
     ax1.set_title(t)
-    plt.imshow(cv2.cvtColor(img1, cv2.COLOR_BGR2RGB))
-    ax2 = fig.add_subplot(rows, columns, 2)
+    ax1.imshow(cv2.cvtColor(img1, cv2.COLOR_BGR2RGB))
     ax2.set_title(s)
-    plt.imshow(cv2.cvtColor(img2, cv2.COLOR_BGR2RGB))
+    ax2.imshow(cv2.cvtColor(img2, cv2.COLOR_BGR2RGB))
     plt.show()
 
 
-alpha_1 = read_csv("Results/flower/Clase_0/alpha.csv").to_numpy()
-mu_1 = read_csv("Results/flower/Clase_0/mu.csv").to_numpy()
-alpha_2 = read_csv("Results/flower/Clase_1/alpha.csv").to_numpy()
-mu_2 = read_csv("Results/flower/Clase_1/mu.csv").to_numpy()
+# alpha_1 = read_csv("Results/flower/Clase_0/alpha.csv").to_numpy()
+# mu_1 = read_csv("Results/flower/Clase_0/mu.csv").to_numpy()
+# alpha_2 = read_csv("Results/flower/Clase_1/alpha.csv").to_numpy()
+# mu_2 = read_csv("Results/flower/Clase_1/mu.csv").to_numpy()
+# alpha = (alpha_1, alpha_2)
+# mu = (mu_1, mu_2)
+
 shape = loadtxt("Results/flower/H_0.txt",
                 max_rows=1,
                 dtype=int)
 h_0 = loadtxt("Results/flower/H_0.txt",
               skiprows=1)
 h_0 = h_0.reshape(shape)
-h_1 = loadtxt("Results/flower/H_0.txt",
+h_1 = loadtxt("Results/flower/H_1.txt",
               skiprows=1)
 h_1 = h_1.reshape(shape)
-alpha = (alpha_1, alpha_2)
-mu = (mu_1, mu_2)
 img = cv2.imread("Data/flower.bmp")
 image = image_model()
 # img_seg = image.segmentation(img, 3, alpha, mu, 0.01, 10)
